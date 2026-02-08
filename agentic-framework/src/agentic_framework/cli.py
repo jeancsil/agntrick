5085b0b168d3af636d27343ba541ba821c847f1c
Jean Silva <jeancsil@gmail.com>
2026-02-08 13:46:47 +0100

    chore: python lint

--- agentic-framework/src/agentic_framework/cli.py
+++ agentic-framework/src/agentic_framework/cli.py
@@ -6,7 +6,6 @@ from rich.console import Console
 from rich.panel import Panel
 
 from agentic_framework.constants import LOGS_DIR
-
 from agentic_framework.registry import AgentRegistry
 
 app = typer.Typer(
