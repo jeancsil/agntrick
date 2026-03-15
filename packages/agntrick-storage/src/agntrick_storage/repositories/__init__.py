18b5d39a6853b9af3bd59a16d97642721c68cae5
Jean Silva <jeancsil@gmail.com>
2026-03-15 11:20:37 +0100

    chore: remove agntrick-storage package

--- packages/agntrick-storage/src/agntrick_storage/repositories/__init__.py
+++
@@ -1,6 +0,0 @@
-"""Repository layer for database access."""
-
-from agntrick_storage.repositories.note_repository import NoteRepository
-from agntrick_storage.repositories.task_repository import TaskRepository
-
-__all__ = ["NoteRepository", "TaskRepository"]
